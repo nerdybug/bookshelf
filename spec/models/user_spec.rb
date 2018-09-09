0a91702dcea03829ead4d0e5c9d5687bda855f23
Meredith Reece <nerdybug@gmail.com>
2018-09-09 19:29:56 -0400

    add user tests for secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe User, type: :model do
   it 'has a password' do
     expect(User.new).to respond_to(:password)
   end
+
+  describe 'authenticated with secure password' do
+    it 'returns a User if the password is correct' do
+      user = User.new
+      user.password = 'foo'
+      expect(user.authenticate('foo')).to eq(user)
+    end
+
+    it 'returns false if password is not correct' do
+      user = User.new
+      user.password = 'foo'
+      expect(user.authenticate('fo0')).to be_falsey
+    end
+  end
 end
