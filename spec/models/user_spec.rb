ce6147b1a0c7adffc7d20529228afa92571488bf
Meredith Reece <nerdybug@gmail.com>
2018-09-09 19:28:01 -0400

    add user tests for name and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'has a name' do
+    expect(User.new).to respond_to(:name)
+  end
 
+  it 'has a password' do
+    expect(User.new).to respond_to(:password)
+  end
 end
