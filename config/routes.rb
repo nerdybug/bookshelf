cde7cc11309c02cf9654fc457da016932df3d008
Meredith Reece <nerdybug@gmail.com>
2018-09-15 18:21:12 -0400

    condense existing resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :authors
-  resources :books
-  resources :users
+  resources :users, :books, :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
