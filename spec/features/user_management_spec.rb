1b3cda8217e188cf8b668f2aac27f35a48481b55
Meredith Reece <nerdybug@gmail.com>
2018-09-30 21:22:41 -0400

    add spec for viewing a user

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,4 +10,10 @@ RSpec.feature "User management", :type => :feature do
 
         expect(page).to have_content("Welcome to your bookshelf, test_user")
     end
+
+    scenario "User views their info" do
+        user = User.create(name: "book owner", password: "owner!")
+        visit "/users/#{user.id}"
+        expect(page).to have_content("Edit your info")
+    end
 end
