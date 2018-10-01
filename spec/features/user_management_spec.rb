3feeff27db733240946b47045577e5268b5da556
Meredith Reece <nerdybug@gmail.com>
2018-09-30 21:25:04 -0400

    edit local variable location

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "User management", :type => :feature do
+    user = User.create(name: "test_user", password: "testing123")
+
     scenario "Visitor creates a new user" do
         visit '/users/new'
 		
-        fill_in "name", :with => "test_user"
-        fill_in "password", :with => "testing123"
+        fill_in "name", :with => user.name
+        fill_in "password", :with => user.password
         click_button "Create User"
 
         expect(page).to have_content("Welcome to your bookshelf, test_user")
     end
 
     scenario "User views their info" do
-        user = User.create(name: "book owner", password: "owner!")
         visit "/users/#{user.id}"
         expect(page).to have_content("Edit your info")
+    end
+
+    scenario "User edits their info" do
+
     end
 end
