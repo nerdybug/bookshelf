9baaef882b298b91a69a4ae6554aeaa8c0e7a07a
Meredith Reece <nerdybug@gmail.com>
2018-09-30 21:43:54 -0400

    add spec for creating a book

--- spec/features/book_management_spec.rb
+++ spec/features/book_management_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Book management", :type => :feature do
+    scenario "A user creates a book" do
+        visit "/books/new"
 
+        fill_in "title", :with => "Interview with the Vampire"
+        fill_in "author", :with => "Anne Rice"
+        click_button "Add book"
+
+        expect(page).to have_content("Interview with the Vampire")
+    end
 end
