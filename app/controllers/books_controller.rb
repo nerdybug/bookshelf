57b7673fdd2f6e60c9e5f78fefa68c1db6e4155c
Meredith Reece <nerdybug@gmail.com>
2018-11-07 21:48:59 -0500

    add private params method, add create method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,17 @@ class BooksController < ApplicationController
     def new
         @book = Book.new
     end
+
+    def create
+        @book = Book.create(book_params.except(:author))
+        @author = Author.create(name: book_params[:author])
+        @book.update(author_id: @author.id)
+        redirect_to book_path(@book)
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :pub_year, :note, :favorite)
+    end
 end
