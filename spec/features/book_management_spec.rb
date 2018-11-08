24ca0bce2b2c120c5ed53d51235879d5091634c0
Meredith Reece <nerdybug@gmail.com>
2018-11-07 21:24:59 -0500

    edit field name for creating book test

--- spec/features/book_management_spec.rb
+++ spec/features/book_management_spec.rb
@@ -8,8 +8,8 @@ RSpec.feature "Book management", :type => :feature do
     scenario "A user creates a book" do
         visit "/books/new"
 
-        fill_in "title", :with => "Interview with the Vampire"
-        fill_in "author", :with => "Anne Rice"
+        fill_in "book[title]", :with => "Interview with the Vampire"
+        fill_in "book[author]", :with => "Anne Rice"
         click_button "Add book"
 
         expect(page).to have_content("Interview with the Vampire")
