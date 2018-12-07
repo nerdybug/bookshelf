560a30290c3d4e009a246639b232d75d7f270b44
Meredith Reece <nerdybug@gmail.com>
2018-12-06 22:19:45 -0500

    add session new and create to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users, :books, :authors
   root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
