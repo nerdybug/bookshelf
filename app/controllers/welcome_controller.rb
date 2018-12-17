2f2c96c82c8d9bda1647f8b2c30f2af9f64c1244
Meredith Reece <nerdybug@gmail.com>
2018-12-16 19:52:58 -0500

    use redirect instead of render

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
     def index
         if session[:user_id]
-            render :'users/home'
+            @user = User.find(session[:user_id])
+            redirect_to user_path(@user)
         else
             render :index
         end
