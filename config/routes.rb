f376325aee44232ebb948e0e076315adf2282105
Meredith Reece <nerdybug@gmail.com>
2018-12-16 19:51:42 -0500

    signup and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :users, :books, :authors
   root 'welcome#index'
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
