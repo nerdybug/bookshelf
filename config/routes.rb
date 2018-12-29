8779e43147786c46744bf043f027cf2ebae652b6
Meredith Reece <nerdybug@gmail.com>
2018-12-28 22:46:02 -0500

    remove post signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/signup', to: 'users#new'
-  post '/signup', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
