e239748ba33ed9fabfa0773b6fa84148656b0520
Meredith Reece <nerdybug@gmail.com>
2019-01-12 21:42:00 -0500

    add destroy user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,14 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        current_user
+        @user.destroy
+        session.delete :user_id
+
+        redirect_to root_path
+    end
+
     private
 
     def user_params
