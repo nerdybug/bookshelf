59cdc2701d395501003c2ae891c13b74d49b7ba8
Meredith Reece <nerdybug@gmail.com>
2019-02-20 21:25:50 -0500

    add missing underscore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_many :books
     has_many :authors, through: :books
-    validates_presence of :name, :password
+    validates_presence_of :name, :password
 end
