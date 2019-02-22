f793ec7713ec70c6a840b11e40cd57ca5d8a9b4d
Meredith Reece <nerdybug@gmail.com>
2019-02-21 20:46:49 -0500

    add configuration to fix field_with_errors styling changes

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Bookshelf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    } # to fix the styling changes that happen with field_with_errors, from SO question #5267998
   end
 end
