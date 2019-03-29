3c9340ca2654a4056baf5fcb8f1984c9356d7ea7
Meredith Reece <nerdybug@gmail.com>
2019-03-28 22:06:05 -0400

    rework user update, still has issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,16 +26,29 @@ class UsersController < ApplicationController
 
     def update
         current_user
-        if !user_params[:password].empty?
-            @user.update(password: user_params[:password])
-            redirect_to user_path(@user)
-        elsif user_params[:name] != @user.name
-            @user.update(name: user_params[:name])
-            redirect_to user_path(@user)
-        else
+        if params[:user][:name] == @user.name && !params[:user][:password].present?
             flash[:no_changes] = "You did not make any changes. Please try again."
             redirect_to edit_user_path(@user)
         end
+        if !params[:user][:password].empty?
+            if !params[:user][:password] == @user.password
+                @user.update(password: params[:user][:password])
+                @user.save
+                flash[:password_updated] = "Your new password has been saved."
+                # this is broken
+            else
+                flash[:same_password] = "The password you entered is your current password. Please try again"
+                redirect_to edit_user_path(@user)
+            end
+        elsif params[:user][:name] != @user.name
+            @user.update(name: params[:user][:name])
+            @user.save
+            flash[:name_updated] = "Your new user name has been saved. #{@user.name}"
+            render 'show' #redirect_to user_path(@user)
+            # this shows the right name but doesnt carry back to home page
+            # reloading the page reverts to the old user name
+        end
+        
     end
 
     def destroy
