ef260a725ea45302dd360bd96d3027ee6d50cac8
Meredith Reece <nerdybug@gmail.com>
2019-04-28 21:07:02 -0400

    reworking to use seeded data

--- spec/features/book_management_spec.rb
+++ spec/features/book_management_spec.rb
@@ -1,44 +1,52 @@
 require 'rails_helper'
 
 RSpec.feature "Book management", :type => :feature do
-    user = User.create(name: "test_user", password: "testing123")
-    author = Author.create(name: "Anne Rice")
-    book = Book.create(user_id: user.id, title: "Interview with the Vampire", author_id: author.id)
+    # user = User.create(name: "test_user", password: "testing123")
+    # author = Author.create(name: "Anne Rice")
+    # book = Book.create(user_id: user.id, title: "Interview with the Vampire", author_id: author.id)
 
-    before(:each) do
+    before(:all) do
+        Rails.application.load_seed
+        @user = User.find_by(name: "test_user1")
+        @book = Book.find_by(title: "test_book1")
         visit "/login"
-        fill_in "user[name]", :with => user.name
-        fill_in "user[password]", :with => user.password
+        fill_in "user[name]", :with => @user.name
+        fill_in "user[password]", :with => @user.password
         click_button "Login"
     end
 
-    scenario "A user creates a book" do      
-        visit "/users/#{user.id}/books/new"
+    # before(:each) do
 
-        fill_in "book[title]", :with => "The Outsider"
-        fill_in "book[author]", :with => "Stephen King"
-        click_button "Add book"
+    # end
 
-        expect(page).to have_content("The Outsider")
-    end
+    feature "Book Management" do
+        scenario "A user creates a book" do     
+            visit "/users/#{@user.id}/books/new"
+            fill_in "book[title]", :with => "The Outsider"
+            fill_in "book[author]", :with => "Stephen King"
+            click_button "Add book"
 
-    scenario "A user views one of their books" do
-        visit "/users/#{user.id}/books/#{book.id}"
-        expect(page).to have_content(book.title)
-    end
+            expect(page).to have_content("The Outsider")
+        end
 
-    scenario "A user edits one of their books" do
-        visit "/books/#{book.id}/edit"
+        scenario "A user views one of their books" do
+            visit "/users/#{@user.id}/books/#{@book.id}"
+            expect(page).to have_content(@book.title)
+        end
 
-        fill_in "book[pub_year]", :with => "1976"
-        click_button "Save"
+        scenario "A user edits one of their books" do
+            visit "/books/#{book.id}/edit"
 
-        expect(page).to have_content("1976")
-    end
+            fill_in "book[pub_year]", :with => "1976"
+            click_button "Save"
 
-    scenario "A user deletes one of their books" do
-        visit "/books/#{book.id}/edit"
-        click_link "Delete Book"
-        expect(page).not_to have_content(book.title)
+            expect(page).to have_content("1976")
+        end
+
+        scenario "A user deletes one of their books" do
+            visit "/books/#{book.id}/edit"
+            click_link "Delete Book"
+            expect(page).not_to have_content(book.title)
+        end
     end
 end
