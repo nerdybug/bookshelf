4b5adfbe1ea95b79b994da988416ef8975788a0a
Meredith Reece <nerdybug@gmail.com>
2019-04-28 21:06:18 -0400

    add seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_list = [
+    ["test_user1", "testing1"],
+    ["test_user2", "testing2"]
+]
+
+user_list.each do |name, password|
+    User.create(name: name, password: password)
+end
+
+author_list = [
+    "test_author1", 
+    "test_author2"
+]
+
+author_list.each do |name|
+    Author.create(name: name)
+end
+test_user1 = User.find_by(name: "test_user1")
+test_user2 = User.find_by(name: "test_user2")
+test_author1 = Author.find_by(name: "test_author1")
+test_author2 = Author.find_by(name: "test_author2")
+book_list = [
+    ["test_book1", test_user1.id, test_author1.id], 
+    ["test_book2", test_user2.id, test_author2.id]
+]
+
+book_list.each do |title, user_id, author_id|
+    Book.create(title: title, user_id: user_id, author_id: author_id)
+end
