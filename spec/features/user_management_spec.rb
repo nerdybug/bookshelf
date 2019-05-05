d52d5d5b8c667964613e4bc1e9e4f5b8d8304a61
Meredith Reece <haku@merediths-mbp.attlocal.net>
2019-05-04 23:07:56 -0400

    use rack session access, load test seeds

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
+require 'securerandom'
 
 RSpec.feature "User management", :type => :feature do
+
+    before(:all) do
+        Rails.application.load_seed
+
+        @user = User.find_by(name: "test_user1")
+        expect(@user).to have_attributes(:name => "test_user1")
+
+        @book = Book.find_by(title: "test_book1")
+        expect(@book).to have_attributes(:title => "test_book1")
+    end
 
     scenario "Visitor sees the welcome page" do
         visit root_url
@@ -12,32 +23,40 @@ RSpec.feature "User management", :type => :feature do
 
     scenario "Visitor creates a new user" do
         visit '/signup'
-		
-        fill_in "user[name]", :with => "test_user1"
-        fill_in "user[password]", :with => "testing123"
-        click_button "Signup"
-
-        expect(page).to have_content("Bookshelf of test_user1")
+        
+        random_username = SecureRandom.uuid
+        random_userpass = SecureRandom.uuid
+        
+        fill_in "user[name]", :with => random_username
+        fill_in "user[password]", :with => random_userpass
+        click_button "Create User"
+        
+        expect(page).to have_content("Bookshelf of #{random_username}")
     end
 
     scenario "Visitor sees their home page after logging in" do
     end
 
     scenario "User views their info" do
-        visit "/users/#{user.id}"
-        expect(page).to have_content("Account Info for #{user.name}")
+        page.set_rack_session(user_id: @user.id)
+        visit "/users/#{@user.id}"
+        expect(page).to have_content("Account Info for #{@user.name}")
     end
 
     scenario "User edits their info" do
-        visit "users/#{user.id}/edit"
-        fill_in "name", :with => "best_user"
+        page.set_rack_session(user_id: @user.id)
+        visit "users/#{@user.id}/edit"
+        fill_in "user[name]", :with => "best_user"
         click_button "Update"
 
         expect(page).to have_content("best_user")
     end
 
     scenario "User deletes their account" do
-        visit "users/#{user.id}/delete"
-        expect(page).to have_content("Welcome to bookshelf!")
+        page.set_rack_session(user_id: @user.id)
+        visit "users/#{@user.id}/edit"
+        
+        click_link "Delete Account"
+        expect(page).to have_content("Welcome to the Bookshelf")
     end
 end
