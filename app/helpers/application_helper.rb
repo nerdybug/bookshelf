867af1b5e9f202b05137293b7211677e1ad357a4
Meredith Reece <haku@merediths-mbp.attlocal.net>
2019-05-15 20:15:35 -0400

    create date and time display methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def date_display(object, created_or_updated)
+        if created_or_updated == "created"
+            object.created_at.strftime("%m-%d-%Y")
+        elsif created_or_updated == "updated"
+            object.updated_at.strftime("%m-%d-%Y")
+        end
+    end
+
+    def time_display(object, created_or_updated)
+        if created_or_updated == "created"
+            created_time = object.created_at.in_time_zone("EST")
+            created_time.strftime("%I:%M %p %Z")
+        elsif created_or_updated == "updated"
+            updated_time = object.updated_at.in_time_zone("EST")
+            updated_time.strftime("%I:%M %p %Z")
+        end
+    end
 end
