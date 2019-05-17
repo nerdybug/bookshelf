e7f1dab760701f23f3f7b0fbf66731b50765a756
Meredith Reece <haku@merediths-mbp.attlocal.net>
2019-05-16 21:00:57 -0400

    move #current_book from books controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
         @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+    def current_book
+        @book = Book.find(params[:id])
+    end
+
     def logged_in?
         if !current_user
             redirect_to login_path
